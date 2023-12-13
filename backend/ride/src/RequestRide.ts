2bf538d036392390eef1ce342ca2ccc94d4d3c27
rodrigorvsn <rodrigovictor81@gmail.com>
2023-12-12 23:49:33 -0300

    chore(clean-arch): add type to Input and Output of Signup and RequestRide

--- backend/ride/src/RequestRide.ts
+++ backend/ride/src/RequestRide.ts
@@ -3,17 +3,42 @@ import { RideRepository } from "./RideRepository";
 import { AccountRepository } from "./AccountRepository";
 import Ride from "./Ride";
 
-export class RequestRide {
-  constructor(private rideRepository: RideRepository, private accountRepository: AccountRepository, private logger: Logger) { }
+type Input = {
+  passengerId: string;
+  fromLat: number;
+  fromLong: number;
+  toLat: number;
+  toLong: number;
+};
 
-  async execute(input: any) {
+type Output = {
+  rideId: string;
+};
+
+export class RequestRide {
+  constructor(
+    private rideRepository: RideRepository,
+    private accountRepository: AccountRepository,
+    private logger: Logger
+  ) { }
+
+  async execute(input: Input): Promise<Output> {
     this.logger.log(`requestRide`);
-    const account = await this.accountRepository.getById(input.passengerId)
-    if (!account) throw new Error('Account does not exists')
-    if (!account.isPassenger) throw new Error('Only passengers can request a ride')
-    const activeRide = await this.rideRepository.getActiveRideByPassengerId(input.passengerId)
-    if (activeRide) throw new Error('Passenger has an active ride')
-    const ride = Ride.create(input.passengerId, input.fromLat, input.fromLong, input.toLat, input.toLong)
+    const account = await this.accountRepository.getById(input.passengerId);
+    if (!account) throw new Error("Account does not exists");
+    if (!account.isPassenger)
+      throw new Error("Only passengers can request a ride");
+    const activeRide = await this.rideRepository.getActiveRideByPassengerId(
+      input.passengerId
+    );
+    if (activeRide) throw new Error("Passenger has an active ride");
+    const ride = Ride.create(
+      input.passengerId,
+      input.fromLat,
+      input.fromLong,
+      input.toLat,
+      input.toLong
+    );
     await this.rideRepository.save(ride);
     return { rideId: ride.rideId };
   }
--- backend/ride/src/Signup.ts
+++ backend/ride/src/Signup.ts
@@ -2,19 +2,39 @@ import { Logger } from "./Logger";
 import { Account } from "./Account";
 import { AccountRepository } from "./AccountRepository";
 
+type Input = {
+  name: string;
+  email: string;
+  cpf: string;
+  carPlate?: string;
+  isPassenger?: boolean;
+  isDriver?: boolean;
+};
+
+type Output = {
+  accountId: string;
+}
+
 export class Signup {
-  accountRepository: AccountRepository
-  logger: Logger
+  constructor(
+    private accountRepository: AccountRepository,
+    private logger: Logger
+  ) { }
 
-  constructor(accountRepository: AccountRepository, logger: Logger) {
-    this.accountRepository = AccountRepository
-    this.logger = logger
-  }
-
-  execute = async (input: any): Promise<{ accountId: string }> => {
-    const existingAccount = await this.accountRepository.getByEmail(input.email);
+  execute = async (input: Input): Promise<Output> => {
+    this.logger.log("signup");
+    const existingAccount = await this.accountRepository.getByEmail(
+      input.email
+    );
     if (existingAccount) throw new Error("Duplicated account");
-    const account = Account.create(input.name, input.email, input.cpf, input.carPlate, input.isPassenger, input.isDriver)
+    const account = Account.create(
+      input.name,
+      input.email,
+      input.cpf,
+      input.carPlate || "",
+      !!input.isPassenger,
+      !!input.isDriver
+    );
     await this.accountRepository.save(account);
     return { accountId: account.accountId };
   };
